bcca088ec9f5ed5e971ca2b9c7d1b0b9bac5da93
Artur Bonamigo <146578735+ArturBonamigo@users.noreply.github.com>
2026-07-02 15:31:25 -0300

    Implementa autenticação JWT e endpoints de usuários

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, viewsets
 
-# Create your views here.
+from .serializers import UserRegisterSerializer, UserSerializer, UserAdminSerializer
+from .models import User
+from .permissions import IsAdminProfile
+
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserRegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+class MeView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserAdminSerializer
+    permission_classes = [IsAdminProfile]
+    http_method_names = ['get', 'patch', 'head', 'options']
