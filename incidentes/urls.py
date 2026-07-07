a15246ede464f2bc1d8dd471361c9f8db44e3312
Artur Bonamigo <146578735+ArturBonamigo@users.noreply.github.com>
2026-07-07 16:49:37 -0300

    Implementa modulo core de incidentes

--- incidentes/urls.py
+++ incidentes/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('apps.accounts.urls')),
+    path('api/', include('apps.incidents.urls')),
 ]
